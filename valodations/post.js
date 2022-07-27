3fe443a6a95be77067d77c2e406e1a29fbeeffb3
kirillEvenstar <kirill.ch@theevenstar.net>
2022-07-27 16:17:14 +0300

    dir structure refactoring

--- valodations/post.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-
-export const postValidation = [
-  body('email', 'wrong format email').isEmail(),
-  body('password', 'minimal length for password is 5 symbols').isLength({ min: 5 }),
-  body('name', 'minimal length for password is 2 symbols').isLength({ min: 2 }),
-  body('avatarUrl', 'avatar must be an url').optional().isURL()
-];
