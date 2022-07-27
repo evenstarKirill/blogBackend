7ea9ebb0729c1394c4dfe60090310f193237c7e4
kirillEvenstar <kirill.ch@theevenstar.net>
2022-07-27 17:11:15 +0300

    added upload img feature and handleValidationErrors middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserControllers from './controllers/UserController.js';
 import * as PostController from './controllers/PostController.js';
+import handleValidationErrors from './utils/handleValidationErrors.js'
 
 mongoose
   .connect(
@@ -15,22 +21,35 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('o hi mark');
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'src/uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-app.post('/auth/login', loginValidation, UserControllers.login);
-app.post('/auth/register', registerValidation, UserControllers.register);
-app.get('/auth/me', checkAuth, UserControllers.getMe);
+const upload = multer({ storage });
 
+app.use(express.json());
+app.use('/uploads', express.static('src/uploads'))
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserControllers.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserControllers.register);
+
+app.get('/auth/me', checkAuth, UserControllers.getMe);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.get('/posts', checkAuth, PostController.getAll);
 app.get('/posts/:id', checkAuth, PostController.getOne);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `uploads/${req.file.originalname}`,
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
